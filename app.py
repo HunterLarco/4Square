d4fcf0c0496a3ee80bda9e49232de6c1aec4a462
Hunter John Larco <hjlarco@gmail.com>
2015-03-07 19:39:45 -0500

    Beta Touch Handling

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import webapp2
 import os
 
 
-class MainHandler(webapp2.RequestHandler):
+class Main(webapp2.RequestHandler):
   def get(self):
     template_values = {}
     path = os.path.join(os.path.dirname(__file__), 'main.html')
     self.response.out.write(template.render(path, template_values))
 
 
+class Beta(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'beta.html')
+    self.response.out.write(template.render(path, template_values))
+
+
 app = webapp2.WSGIApplication([
-                ('/.*', MainHandler)
+                ('/beta/?', Beta),
+                ('/.*', Main)
               ], debug=True)
