9c55a825eb50102d503c27280d1f4bdbbd5523fb
Hunter John Larco <hjlarco@gmail.com>
2015-03-09 01:41:55 -0400

    Switched to Beta

--- app.py
+++ app.py
@@ -10,14 +10,6 @@ class Main(webapp2.RequestHandler):
     self.response.out.write(template.render(path, template_values))
 
 
-class Beta(webapp2.RequestHandler):
-  def get(self):
-    template_values = {}
-    path = os.path.join(os.path.dirname(__file__), 'beta.html')
-    self.response.out.write(template.render(path, template_values))
-
-
 app = webapp2.WSGIApplication([
-                ('/beta/?', Beta),
                 ('/.*', Main)
               ], debug=True)
